4d50dc5a867207d08df4597b29be38598c524921
Heeesuu <gmltn0508@icloud.com>
2023-04-19 15:01:00 +0900

    ㅆT를 String으로 변경, get 구현

--- src/main/java/com/ll/exam1/MyArrayList.java
+++ src/main/java/com/ll/exam1/MyArrayList.java
@@ -2,17 +2,21 @@ package com.ll.exam1;
 
 public class MyArrayList<T> {
 
+    private String[] data = new String[2];
     private int size = 0;
+
+
     public int size() {
         return size;
     }
 
-    public boolean add(T element) {
+    public boolean add(String element) {
+        data[size] = element;
         size ++;
         return true;
     }
 
-    public T get(int index) {
-        return (T) "포도";
+    public String get(int index) {
+        return data[index];
     }
 }
